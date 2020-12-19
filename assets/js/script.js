872f6de3df177ad82651287404d28a2da8a1a961
Eric Normann <eric.n@me.com>
2020-12-19 01:40:12 -0500

    update README.md

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,33 @@
 function generatePassword() {
   
   //declare necessary variables
-  let passwordLength;
-  let characters="";
-  let lc;
-  let uc;
-  let num;
-  let sc;
-  let finalPassword = "";
-  let passwordValid;
-
+    //variable for storing how many characters long the pasword will be
+      let passwordLength;
+    //variables for determining what types of characters will be used and storing those characters 
+    //as a resource to generate the password
+      let lc;
+      let uc;
+      let num;
+      let sc;
+      let characters="";
+    //variable for storing the final password
+      let finalPassword = "";
+    //variable for determining if password has at least one of each kind of character
+      let passwordValid;
   
   //request desired password length and store result
   do {
     passwordLength = window.prompt("How long do you want your password be? \nPick a length between 8 and 128 characters.");
+    //convert length into an integer
     passwordLength = parseInt(passwordLength);
     //check to make sure a valid number was chosen and warn user if it was not.
     if (Number.isNaN(passwordLength) === true || passwordLength < 8 || passwordLength > 128) {
       window.alert("Invalid entry. Please try again.");
     } 
-  //Check to make sure a valid number was chosen and repplay prompt if it was not.
+  //Check to make sure a valid number was chosen and redeploy prompt if it was not.
   } while (Number.isNaN(passwordLength) === true || passwordLength < 8 || passwordLength > 128);
   
   //find out what types characters to include as options for password and store them in the characters variable
-  
   do {
     lc = window.confirm("Include lowercase letters in your password? \n(OK for yes. Cancel for no.)");
     uc = window.confirm("Include uppercase letters in your password? \n(OK for yes. Cancel for no.)");
@@ -34,28 +38,28 @@ function generatePassword() {
     if (lc === false && uc === false && num === false && sc === false) {
       window.alert("You must choose at least one of lowercase, uppercase, numbers or special characters. Please try again.");
     }
-  //check to make sure at least one character type was chosen. If not replay the prompts until at least one is.
+  //check to make sure at least one character type was chosen. If not redeploy the confirms until at least one is.
   } while (lc === false && uc === false && num === false && sc === false);
 
   //Check to see what charcter types were chosen and add them to the characters variable
   if (lc===true) {
-    characters = characters + "abdefghijklmnopqrstuvwxyz";
+    characters += "abdefghijklmnopqrstuvwxyz";
   } 
   if (uc===true) {
-    characters = characters + "ABCEDEFGHIJKLMNOPQRSTUVWXYZ";
+    characters += "ABCEDEFGHIJKLMNOPQRSTUVWXYZ";
   }
   if (num === true) {
-    characters = characters + "0123456789";
+    characters += "0123456789";
   }
   if (sc === true) {
-    characters = characters + "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    characters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   }
   do {
     //randomly generate a character and add it to the password until the password is the desired length.
     for (var i = 0; i <passwordLength; i++) {
       finalPassword = finalPassword + characters.charAt(Math.floor(Math.random()*characters.length));
     } 
-    //Check to see that password has at least one of each chosen character type. 
+    //Check to see that password has at least one of each chosen character type. If not reset the finalPassword variable
     if (
            lc  === true && finalPassword.match(/[a-z]/) === null 
         || uc  === true && finalPassword.match(/[A-Z]/) === null 
@@ -64,13 +68,13 @@ function generatePassword() {
        ) {
       passwordValid = false;
       finalPassword="";
-      //console.log("failed");
     } else {
       passwordValid=true;
     }
   // If password does not have one of each chosen character type regenerate the password
   } while (passwordValid === false);
 
+  //return the password.
   return finalPassword;
 
 }
@@ -90,4 +94,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
